898d3c83bbcf32987bc5fe7ff419b34c31bed307
Carla Walter <walterc@gmx.com>
2018-02-01 06:22:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function whisper(string){return string.toLowerCase()}
 function logShout(string){console.log((string).toUpperCase())}
 function logWhisper(string){console.log((string).toLowerCase())}
 function sayHiToGrandma(string){
-  if ((string) === uppercase) {return "YES INDEED!"}
-  if ((string) === lowercase) {return "I can't hear you!"}
+  if ((string) === UpperCase) {return "YES INDEED!"}
+  if ((string) === LowerCase) {return "I can't hear you!"}
   else {return "I love you, Grandma."}}
