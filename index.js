36f10e4e3b1a8115907f7290d52f690856df8c90
Carla Walter <walterc@gmx.com>
2018-02-01 06:17:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function shout(string){return string.toUpperCase()}
 function whisper(string){return string.toLowerCase()}
-function logShout(string){
-  console.log((string).toUpperCase())
-}
+function logShout(string){console.log((string).toUpperCase())}
+function logWhisper(string){console.log((string).toLowerCase())}
